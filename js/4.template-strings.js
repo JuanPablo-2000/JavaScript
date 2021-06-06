4f2453d72c5bbc22fce8c5de5dba8e67c623c20d
JuanPablo-2000 <agudelo.14321@gmail.com>
2021-06-06 17:35:38 -0500

    templates-strings

---
+++ js/4.template-strings.js
@@ -0,0 +1,23 @@
+// Templates Strings
+
+let nombre = "Juan Pablo";
+let apellido = "Mera Agudelo";
+
+// Concatenación 
+let saludo = "Hola mi nombre es " + nombre + " " + apellido + ".";
+console.log(saludo);
+
+// Interpolación de variables
+// Templates Strings
+let saludo2 = `Hola mi nombre es ${nombre} ${apellido}.`;
+console.log(saludo2);
+
+let ul = `
+<ul>
+	<li> Primavera</li>
+	<li> Verano</li>
+	<li> Otoño</li>
+	<li> Invierno</li>
+</ul>`;
+
+console.log(ul);
