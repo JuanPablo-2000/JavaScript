4795478630ff10a5ff009cb21ca329f436b4e7ad
JuanPablo-2000 <agudelo.14321@gmail.com>
2021-06-10 11:25:51 -0500

    break & continue

---
+++ js/_15.break-continue.js
@@ -0,0 +1,18 @@
+const numeros = [1,2,3,4,5,6,7,8,9,0];
+
+console.log("break\n")
+for(let i=0;i<numeros.length;i++) {
+	if(i===5) {
+		break;
+	}
+	console.log(numeros[i]);
+}
+
+console.log("\ncontinue")
+
+for(let i=0;i<numeros.length;i++) {
+	if(i===5) {
+		continue;
+	}
+	console.log(numeros[i]);
+}
