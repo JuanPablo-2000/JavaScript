99916eca8ad2fe0b0f50a87cddc8b17b7f1c31a8
JuanPablo-2000 <agudelo.14321@gmail.com>
2021-06-10 02:34:07 -0500

    manejo-errores

--- js/_14.manejo-errores.js
+++ js/_14.manejo-errores.js
@@ -9,4 +9,16 @@ try {
 	console.log("El bloque finally se ejecutará siempre al final de unbloque try-catch");
 }
 
+try {
+	let numero = "y";
+	if(isNaN(numero)) {
+		//throw lanza el error
+		throw new Error("El caractér introducido no es un Número");
+	}
+
+	console.log(numero * numero);
+} catch(error) {
+	console.log(`Se produjo el siguiente error: ${error}`);
+}
+
 
