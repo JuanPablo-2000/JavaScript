32c239e1555450654ab7451725ee3c8391175657
JuanPablo-2000 <agudelo.14321@gmail.com>
2021-06-10 11:49:07 -0500

    objetos-literales

---
+++ js/_17.objetos-literales.js
@@ -0,0 +1,27 @@
+let nombre = "Morgan",
+	edad = 7;
+
+const perro = {
+	nombre: nombre,
+	edad: edad,
+	
+	ladrar: function (){
+		console.log("guauu guauu!!!");
+	}
+}
+
+console.log(perro);
+perro.ladrar();
+
+const dog = {
+	nombre,
+	edad,
+	raza: "Callejero",
+
+	ladrar() {
+		console.log("guauu guauu guauu !!!");
+	}
+}
+
+console.log(dog);
+dog.ladrar();
