eea612d8a31a70091e08d227691b31c85caba34f
JuanPablo-2000 <agudelo.14321@gmail.com>
2021-06-13 00:15:01 -0500

    objeto-math

---
+++ js/_24.objeto-math.js
@@ -0,0 +1,14 @@
+// Objeto Math 
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.abs(-7.8)); //valor absoluto
+console.log(Math.ceil(7.2)); //función techo
+console.log(Math.floor(7.8)); //función piso
+console.log(Math.round(7.49)); //redondeo.
+console.log(Math.sqrt(81)); //raíz cuadrada
+console.log(Math.pow(2, 5)); //función para elevar
+console.log(Math.sign(12)); //devuelve 1 si es + -1 si es - ó 0 si es zero
+console.log(Math.random()); //número aleatorio 
+console.log(Math.round(Math.random()*1000)); //número aleatorio entre 0 - 1000
+
