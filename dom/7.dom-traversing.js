8199fc5e060496bd9084d2262d51b7f887b960e8
JuanPablo-2000 <agudelo.14321@gmail.com>
2021-06-20 13:46:23 -0500

    dom-traversing

---
+++ dom/7.dom-traversing.js
@@ -0,0 +1,16 @@
+/** DOM - Traversing */
+const $cards = document.querySelector(".cards");
+
+console.log($cards);
+console.log($cards.children)
+console.log($cards.childNodes);
+console.log($cards.children[2]);
+console.log($cards.parentElement);
+console.log($cards.parentNode);
+console.log($cards.firstElementChild);
+console.log($cards.lastElementChild);
+console.log($cards.previousElementSibling);
+console.log($cards.nextElementSibling);
+console.log($cards.closest("div"));
+console.log($cards.closest("body"));
+console.log($cards.children[3].closest("section"));
