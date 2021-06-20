c337a4d4d8a784007962765bba4a3d13087644f8
JuanPablo-2000 <agudelo.14321@gmail.com>
2021-06-19 19:51:15 -0500

    atributos - DataAttributes

---
+++ dom/3.dom-atributos-dataAttibutes.js
@@ -0,0 +1,38 @@
+/** Atributos y Data-Attributes */
+
+console.log(document.documentElement.lang);
+console.log(document.documentElement.getAttribute("lang"));
+
+/** Trae la dirección de broadcast completa */
+console.log(document.querySelector(".link-dom").href);
+
+document.documentElement.lang = "es";
+console.log(document.documentElement.lang);
+
+document.documentElement.setAttribute("lang", "es-MX");
+console.log(document.documentElement.lang);
+
+/* Las variables que tiene el dolar al principio
+ * indican que esa misma variable hace parte del dom
+ */
+const $linkDOM = document.querySelector(".link-dom");
+
+$linkDOM.setAttribute("target", "_blank");
+$linkDOM.setAttribute("rel", "noopener");
+$linkDOM.setAttribute("href", "https://youtube.com/jonmircha");
+
+console.log($linkDOM.hasAttribute("rel"));
+$linkDOM.removeAttribute("rel");
+console.log($linkDOM.hasAttribute("rel"));
+
+// Data-Attributes
+console.log($linkDOM.getAttribute("data-description"));
+console.log($linkDOM.dataset);
+console.log($linkDOM.dataset.description);
+$linkDOM.setAttribute("data-description", "Modelo de Objeto del Documento");
+console.log($linkDOM.dataset.description);
+$linkDOM.dataset.description = "Suscribete a mu canal y comparte";
+console.log($linkDOM.dataset.description);
+console.log($linkDOM.hasAttribute("data-id"));
+console.log($linkDOM.removeAttribute("data-id"));
+console.log($linkDOM.dataset.description);
